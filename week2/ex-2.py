99ab6ca6bd2545ee18cd83b4455e18c89ca2cb20
kperry753 <kperry753@gmail.com>
2021-02-21 20:38:12 -0800

    here be dragons

--- week2/ex-2.py
+++ week2/ex-2.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 from getpass import getpass
+from datetime import datetime
 
 device1 = {
     "host": 'nxos1.lasthop.io',
@@ -9,8 +10,28 @@ device1 = {
     "global_delay_factor": 4,
 }
 
+time = datetime.now()
+
 net_connect = ConnectHandler(**device1)
 print(net_connect.find_prompt())
 
+
+
+
+
+
+
+
+time1 = datetime.now()
 output = net_connect.send_command("show lldp neighbors detail")
 print(output)
+time2 = datetime.now()
+
+time3 = datetime.now()
+output = net_connect.send_command("show lldp neighbors detail", delay_factor=8)
+print(output)
+time4 = datetime.now()
+
+print('\n',time1,'\n',time2)
+
+print('\n',time3,'\n',time4)
